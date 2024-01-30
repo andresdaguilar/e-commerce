a2fe329b631c56c85eeec23e089bda2d923598df
Andrés Aguilar <andresd.aguilar@gmail.com>
2024-01-30 13:54:56 -0300

    new branch

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -18,4 +18,32 @@ export class ProductService {
     const newProduct = new this.productModel({ name, price, description });
     return newProduct.save();
   }
+
+  async findAll(): Promise<ProductDocument[]> {
+    return this.productModel.find().exec();
+  }
+
+  async find(id: string): Promise<ProductDocument> {
+    return this.productModel.findById(id).exec();
+  }
+
+  async update(
+    id: string,
+    newName: string,
+    newPrice: number,
+    newDescription: string,
+  ): Promise<ProductDocument> {
+    const existingProduct = await this.find(id);
+    if (newName) {
+      existingProduct.name = newName ?? existingProduct.name;
+      existingProduct.price = newPrice ?? existingProduct.price;
+      existingProduct.description =
+        newDescription ?? existingProduct.description;
+      return existingProduct.save();
+    }
+  }
+
+  async delete(id: string) {
+    await this.productModel.findByIdAndDelete(id).exec();
+  }
 }
